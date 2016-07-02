b91b7823e837e06d9037581092d18db4ac9005f5
Moghan <anders.kensby@gmail.com>
2016-07-02 11:07:45 +0200

    feat:	Write all tests. Add .eslintrc.json file.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -10,10 +10,10 @@
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
-    describe('RSS Feeds', function() {
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
+	describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,52 +21,128 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
+		it('are defined', function() {
+			expect(allFeeds).toBeDefined();
+			expect(allFeeds.length).not.toBe(0);
+		});
 
-
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+		it('feed URLs are defined and valid', () => {
+			allFeeds.forEach((feed) => {
+				expect(feed.url).toBeDefined();
 
+                // Found the expression in the discussion forum.
+				var regularExpressionUrl = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
+				expect(feed.url).toMatch(regularExpressionUrl);
+			});
+		});
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-    });
-
+		it('feed names are defined and not empty', () => {
+			allFeeds.forEach((feed) => {
+				expect(feed.name).toBeDefined();
+				expect(feed.name).not.toEqual('');
+			});
+		});
+	});
 
     /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
+	describe('The menu', () => {
+		
+        /* Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+		it('menu element is hidden by default', () => {
+			var isMenuHidden = $('body').hasClass('menu-hidden');
+			expect(isMenuHidden).toBe(true);
+		});
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        /* Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+		it('menu changes visibility when icon clicked', () => {
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+			var htmlElement_SlideMenu = $('.slide-menu');
+			var htmlElement_MenuIcon = $('.menu-icon-link');
+			var wasVisibleBeforeClicked;
+			
+			// Assuming menu will dissapear upwards or to the left, or using the ccs style "display = none".
+			var checkIfVisible = function () {
+				return (-htmlElement_SlideMenu.offset().left <= htmlElement_SlideMenu.width())
+					&& (-htmlElement_SlideMenu.offset().top <= htmlElement_SlideMenu.height())
+					&& (htmlElement_SlideMenu.css('display') !== 'none');
+			};
 
-        /* TODO: Write a test that ensures when the loadFeed
+			// With transition on, the slide-menu has not entered the view when test is made.
+			htmlElement_SlideMenu.addClass('notransition');
+
+			wasVisibleBeforeClicked = checkIfVisible();
+			htmlElement_MenuIcon.click();
+			expect(wasVisibleBeforeClicked).not.toBe(checkIfVisible());
+
+			wasVisibleBeforeClicked = checkIfVisible();
+			htmlElement_MenuIcon.click();
+			expect(wasVisibleBeforeClicked).not.toBe(checkIfVisible());
+
+			htmlElement_SlideMenu.removeClass('notransition');
+		});
+	});
+
+    /* Write a new test suite named "Initial Entries" */
+	describe('Initial Entries', () => {
+		
+		beforeEach( (done) => {
+			loadFeed(0, done);
+		});
+
+		/* Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+		it('loadFeed() completes and at least one element is in container', (done) => {
+			var htmlElement_Entries = $('.entry');
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+			expect(htmlElement_Entries.length).toBeGreaterThan(0);
+			done();
+		});
+	});
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+    /* Write a new test suite named "New Feed Selection" */
+	describe('New Feed Selection', () => {
+		var oldContent;
+
+		beforeEach( (done) => {
+			if(allFeeds.length > 1) {
+				loadFeed(0, () => {
+					// Alternative content = $('.feed').html();
+					oldContent = $('.entry h2')[0].textContent;
+					loadFeed(1, done);
+				});
+			}
+		});
+
+        /* Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+		it('changes content when new feed is loaded', (done) => {
+			var newContent = $('.entry h2')[0].textContent;
+
+			expect(allFeeds.length).toBeGreaterThan(1);
+			expect(oldContent).not.toEqual(newContent);
+			done();
+		});
+	});
 }());
